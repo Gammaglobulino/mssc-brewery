39966690e7cdfca8ee9d968047300e74a0ef63ac
DESKTOP-KCS1KCG\Drako <mazzantia@hotmail.com>
2019-09-09 11:05:38 +0200

    Adding a new V2 controller - just demo

--- src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
@@ -1,9 +1,13 @@
 package guru.springframework.msscbrewery.services;
 
+import guru.springframework.msscbrewery.web.model.BeerDto;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
 
 import java.util.UUID;
 
 public interface CustomerService {
+    void updateCustomer(UUID customerId, CustomerDto customerDto);
     CustomerDto getCustomerById(UUID customerId);
+    CustomerDto saveNewCustomer(CustomerDto customerDto);
+    void deleteById(UUID customerId);
 }
--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,13 +1,36 @@
 package guru.springframework.msscbrewery.services;
 
+import guru.springframework.msscbrewery.web.model.BeerDto;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 @Service
+@Slf4j
 public class CustomerServiceImpl implements CustomerService {
     @Override
-    public CustomerDto getCustomerById(UUID customerId) {
-        return CustomerDto.builder().id(UUID.randomUUID()).Name("Andrea Mazzanti").build();
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        // TODO - implement the logic for updating and save a customer
     }
+
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.builder().id(UUID.randomUUID())
+                                    .Name("Andrea Mazzanti")
+                                    .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder().
+                id(UUID.randomUUID()).
+                build();
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting customer...");
+    }
+
 }
